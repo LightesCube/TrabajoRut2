7ec57a0e7ebfb1a805b77ef2eda5ce9cbd371cbe
LightesCube <diegomarillanblooop@gmail.com>
2023-04-19 23:10:36 -0400

    Initial commit

---
+++ src/main/java/TrabajoRut.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+    public class TrabajoRut {
+        public static void main(String[] args) {
+            Scanner sc =new Scanner(System.in);
+            String rut="12345678";
+        }
+        public static String InvertirRut("12345678");
+
+
+    }
+
+
